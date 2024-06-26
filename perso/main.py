fab76aa78b7cb0520c62ab078d21e34dde333430
thierry57360 <thierry57360@gmail.com>
2024-06-26 12:32:27 +0000

    version avec les listes de sélection

---
+++ perso/main.py
@@ -0,0 +1,128 @@
+import dash
+from dash import dcc
+from dash import html
+import dash_leaflet as dl
+from dash.dependencies import Output, Input, State
+from FlightRadar24 import FlightRadar24API
+from utils import (
+    update_rotation_angles,
+    get_closest_round_angle,
+    get_custom_icon,
+    fetch_flight_data
+)
+
+
+# App initialization
+app = dash.Dash(__name__)
+# FlightRadar24API client
+fr_api = FlightRadar24API()
+
+
+default_map_children = [
+    dl.TileLayer()
+]
+
+# airlines list
+zones = fr_api.get_airlines()
+icao_list = [item['ICAO'] for item in zones]
+company_name = [item['Name'] for item in zones]
+
+# Mapping ICAO codes to company names
+icao_to_name = {item['ICAO']: item['Name'] for item in zones}
+
+app.layout = html.Div([
+    # The memory store reverts to the default on every page refresh
+    dcc.Store(id="memory"),
+    dcc.Dropdown(
+        id='zone-dropdown',
+        options=[{'label': zone, 'value': zone} for zone in fr_api.get_zones().keys()],
+        value='europe'  # Valeur par défaut
+    ),
+    dcc.Dropdown(
+        id='company-dropdown',
+        options=[{'label': icao_to_name[icao_choice], 'value': icao_choice} for icao_choice in icao_list],
+        value='AFR'  # Valeur par défaut
+    ),
+
+
+    # The local store will take the initial data
+    # only the first time the page is loaded
+    # and keep it until it is cleared.
+    dcc.Store(id="local", storage_type="local"),
+    # Same as the local store but will lose the data
+    # when the browser/tab closes.
+    dcc.Store(id="session", storage_type="session"),
+    # TO MODIFY
+    dl.Map(
+        id='map',
+        center=[56, 10],
+        zoom=6,
+        style={'width': '100%', 'height': '800px'},
+        children=default_map_children
+    ),
+    dcc.Interval(
+        id="interval-component",
+        interval=2*1000,  # in milliseconds 
+        n_intervals=0
+    )
+])
+
+
+
+@app.callback(
+    [Output('map', 'children'), Output('memory', 'data')],
+    [Input('interval-component', 'n_intervals'), Input('zone-dropdown', 'value'), Input('company-dropdown', 'value')],
+    [State('memory', 'data')]
+)
+ #data = fetch_flight_data(client=fr_api, airline_icao="AFR", zone_str=zone)
+# mise à jour
+def update_graph_live(n, zone, airline_company, before_d):
+  
+    data = fetch_flight_data(client=fr_api, airline_icao= airline_company, zone_str=zone)
+    
+    if before_d is None:
+        for flight_data in data:
+            flight_data.update(rotation_angle=0)
+    else:
+        update_rotation_angles(data, before_d)
+
+    # Update map children by adding markers to the default tiles layer
+    children = default_map_children + [
+        dl.Marker(
+            id=flight['id'],
+            position=[flight['latitude'], flight['longitude']],
+            children=[
+                dl.Popup(html.Div([
+                    dcc.Markdown(f'''
+                        **Identifiant du vol**: {flight['id']}.
+
+                         **numéro du vol**: {flight['number']}.
+
+                        **Aérport d'origine**: {flight['origin_airport_iata']}.
+
+                        **Aéroport de destination**: {flight['destination_airport_iata']}.
+
+                        **Vitesse au sol**: {round(flight['ground_speed'] *1.852) } Km/h.
+
+                        **Vitesse verticale**: {round(flight['vertical_speed'] * 0.3048, 2)} m/s.
+
+                        **altitude**: {round(flight['altitude'] * 0.3048)} m.
+
+                        **Position**: {'en vol' if flight['on_ground'] == 0 else 'au sol'}.
+
+                    ''')
+                ]))
+            ],
+            icon=get_custom_icon(
+                get_closest_round_angle(flight['rotation_angle'])
+            ),
+        ) for flight in data
+    ]
+
+    return [children, data]
+
+
+if __name__ == '__main__':
+    app.run_server(
+        debug=True, port=5000, host='0.0.0.0'
+    )
---
+++ perso/utils.py
@@ -0,0 +1,169 @@
+"""
+Utils.
+"""
+from typing import Dict, Optional, List
+import math
+import numpy as np
+from FlightRadar24 import FlightRadar24API
+
+
+def fetch_flight_data(
+    client: FlightRadar24API,
+    airline_icao: Optional[str] = None,
+    aircraft_type: Optional[str] = None,
+    zone_str: Optional[str] = None
+) -> List[Dict]:
+    """
+    Fetch flight data from FlightRadar24 API for
+    a given airline, aircraft type and zone.
+
+    Args:
+        client (FlightRadar24API): FlightRadar24API client.
+        airline_icao (str): ICAO code of the airline.
+        aircraft_type (str): Type of aircraft.
+        zone_str (str): Zone string.
+
+    Returns:
+        List[Dict]: List of flights. A flight should be represented
+            as a dictionary with latitude, longitude, id and additional
+            keys.
+    """
+    zone = client.get_zones()[zone_str]
+    bounds = client.get_bounds(zone)
+
+    flights = client.get_flights(
+        aircraft_type=aircraft_type,
+        airline=airline_icao,
+        bounds=bounds
+    )
+    return [
+        {
+            "latitude": flight.latitude,
+            "longitude": flight.longitude,
+            "id": flight.id,
+            "number": flight.number,
+            "origin_airport_iata": flight.origin_airport_iata,
+            "destination_airport_iata": flight.destination_airport_iata,
+            "ground_speed": flight.ground_speed,
+            "on_ground": flight.on_ground,
+            "altitude": flight.altitude,
+            "vertical_speed": flight.vertical_speed
+        } for flight in flights
+    ]
+
+
+def update_rotation_angles(data: Dict, previous_data: Dict) -> None:
+    """
+    mise à jour d'angle de rotation des avions
+    """
+    for idx, flight_data in enumerate(data):
+        identifier = flight_data["id"]
+        if identifier not in [data["id"] for data in previous_data]:
+            bearing = 0
+        else:
+            previous_flight_data = next(item for item in previous_data if item["id"] == identifier)
+            longitude = flight_data["longitude"]
+            latitude = flight_data["latitude"]
+            previous_longitude = previous_flight_data["longitude"]
+            previous_latitude = previous_flight_data["latitude"]
+
+            # pas de changement 
+            if (longitude == previous_longitude) & (latitude == previous_latitude):
+                bearing = previous_flight_data["rotation_angle"]
+            else:
+                bearing = bearing_from_positions(
+                    longitude,
+                    latitude,
+                    previous_longitude,
+                    previous_latitude,
+                )
+
+        flight_data.update(rotation_angle=bearing)
+    return
+
+
+def bearing_from_positions(
+    longitude: float,
+    latitude: float,
+    previous_longitude: float,
+    previous_latitude: float,
+) -> float:
+    """
+    Compute bearing from two sets of coordinates, at
+    t-1 and t. Bearing is measured clockwise from the north.
+
+    Args:
+        longitude (float): Longitude (in degrees).
+        latitude (float): Latitude (in degrees).
+        previous_longitude (float): Previous longitude (in degrees).
+        previous_latitude (float): Previous latitude (in degrees).
+
+    Returns:
+        float: Bearing in degrees.
+    """
+    # Convert to radians
+    lat1, lon1, lat2, lon2 = map(
+        math.radians,
+        [previous_latitude, previous_longitude, latitude, longitude]
+    )
+    # Compute the difference between the two longitudes
+    dlon = lon2 - lon1
+    # Compute the initial bearing
+    y = math.sin(dlon) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dlon)
+    bearing_rad = math.atan2(y, x)
+    # Convert the bearing to degrees
+    bearing_deg = math.degrees(bearing_rad)
+    # Ensure the bearing is between 0 and 360 degrees
+    bearing_deg = (bearing_deg + 360) % 360
+    return bearing_deg
+
+
+def get_closest_round_angle(angle: float) -> int:
+    """
+    Get closest round angle (multiple of 15 degrees)
+    to the given angle.
+
+    Args:
+        angle (float): Given angle.
+
+    Returns:
+        int: Closest angle among the following values:
+            0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150,
+            165, 180, 195, 210, 225, 240, 255, 270,
+            285, 300, 315, 330, 345.
+    """
+    round_angles = [
+        0, 15, 30, 45, 60, 75, 90,
+        105, 120, 135, 150, 165, 180,
+        195, 210, 225, 240, 255, 270,
+        285, 300, 315, 330, 345, 360,
+    ]
+    differences = np.array([
+        angle - round_angle for round_angle in round_angles
+    ])
+    abs_differences = np.abs(differences)
+    closest_angle_idx = abs_differences.argmin()
+    closest_angle = round_angles[closest_angle_idx]
+    if closest_angle == 360:
+        return 0
+    else:
+        return closest_angle
+
+
+def get_custom_icon(round_angle: int) -> Dict:
+    """
+    Get custom plane icon corresponding to the
+    given round_angle.
+
+    Args:
+        round_angle (int): Round angle.
+
+    Returns:
+        Dict: Icon dict.
+    """
+    icon_url = f'https://github.com/tomseimandi/flightradar/blob/main/img/plane_{round_angle}.png?raw=true'
+    return dict(
+        iconUrl=icon_url,
+        iconSize=[38, 38],
+    )
